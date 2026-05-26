dcd9b3aea13dfd2a7ecc2dbcb240d65d7dc83dd9
grandmaester <rohitsharma123902@gmail.com>
2026-05-26 15:01:11 +0530

    feat: pushing view for members and member details pages

--- app/dashboard/organization/members/page.tsx
+++ app/dashboard/organization/members/page.tsx
@@ -1,11 +1,21 @@
 "use client"
 
+import { useRouter } from "next/navigation"
 import { OrganizationMemberManagement } from "@auth0/universal-components-react"
 
 export default function Members() {
+  const router = useRouter()
   return (
     <div className="space-y-2 p-6">
-      <OrganizationMemberManagement />
+      <OrganizationMemberManagement
+        viewMemberDetailsAction={{
+          onAfter: (userId) => {
+            router.push(
+              `/dashboard/organization/members/${encodeURIComponent(userId)}`
+            )
+          },
+        }}
+      />
     </div>
   )
 }
