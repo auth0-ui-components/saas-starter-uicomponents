ba79219de8d2f7f61b1444ee844565b5b7fe6a8a
grandmaester <rohitsharma123902@gmail.com>
2026-05-26 12:18:24 +0530

    feat: member flow changes with tarball updates

--- scripts/utils/clients.mjs
+++ scripts/utils/clients.mjs
@@ -57,7 +57,11 @@ const MANAGEMENT_API_SCOPES = [
   "read:my_org:member_invitations",
   "delete:my_org:member_invitations",
   "create:my_org:member_invitations",
-  "read:my_org:member_roles"
+  "read:my_org:member_roles",
+  "delete:my_org:member_roles",
+  "create:my_org:member_roles",
+  "read:my_org:members",
+  "delete:my_org:memberships"
 ]
 
 // ============================================================================
